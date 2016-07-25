ef9125fb7c253f3462a6c8410e52a3d92e2d6c0b
HFCherish <hf_cherish@163.com>
2016-07-25 19:13:32 +0800

    modified db connection to use localhost in local

--- src/main/java/com/thoughtworks/ketsu/Dao/BasicDao.java
+++ src/main/java/com/thoughtworks/ketsu/Dao/BasicDao.java
@@ -9,12 +9,28 @@ import java.net.UnknownHostException;
 public class BasicDao {
     private static DB db;
 
+    private static String getConnectUrl() {
+        String dbname = System.getenv().getOrDefault("MONGODB_DATABASE", "mongodb_store");
+        String host = System.getenv().getOrDefault("MONGODB_HOST", "localhost");
+        String port = System.getenv().getOrDefault("MONGODB_PORT", "27017");
+        String username = System.getenv().getOrDefault("MONGODB_USER", "admin");
+        String password = System.getenv().getOrDefault("MONGODB_PASS", "mypass");
+        String connectURL = String.format(
+                "mongodb://%s:%s@%s/%s",
+                username,
+                password,
+                host,
+                dbname
+        );
+        return connectURL;
+    }
+
     public static DB getDB()  {
         if(db == null) {
             MongoClient mongoClient = null;
             try {
                 mongoClient = new MongoClient(
-                        new MongoClientURI("mongodb://admin:mypass@localhost/mongodb_store"));
+                        new MongoClientURI(getConnectUrl()));
             } catch (UnknownHostException e) {
                 e.printStackTrace();
             }
